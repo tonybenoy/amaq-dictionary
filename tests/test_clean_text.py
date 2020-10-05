8f71c06b4eab053274cefb13838fec69ba284280
Tony Benoy <me@tonybenoy.com>
2020-10-05 19:15:54 +0530

    missed type hints

--- tests/test_clean_text.py
+++ tests/test_clean_text.py
@@ -1,21 +1,21 @@
 from helper import clean_text
 
 
-def test_clean_text_no_special_char():
+def test_clean_text_no_special_char() -> None:
     text = clean_text("hello world")
     assert text == "hello world"
 
 
-def test_clean_text_digits():
+def test_clean_text_digits() -> None:
     text = clean_text("hello world123")
     assert text == "hello world"
 
 
-def test_clean_text_special_char():
+def test_clean_text_special_char() -> None:
     text = clean_text("hello world#$%")
     assert text == "hello world"
 
 
-def test_clean_text_multiple_spaces():
+def test_clean_text_multiple_spaces() -> None:
     text = clean_text("hello world   ")
     assert text == "hello world"
